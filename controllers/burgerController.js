6cbd76452e76cef82ad41c747f9f128f717f6677
Janani NA <janani2285@gmail.com>
2020-07-18 13:44:31 -0700

    Initial commit

---
+++ controllers/burgerController.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+// Import the model (cat.js) to use its database functions.
+//const cat = require("../models/cat.js");
+
+const router = express.Router();
+// Export routes for server.js to use.
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Meeses');
+INSERT INTO burgers (name) VALUES ('Ham burger');
+INSERT INTO burgers (name, isDevour) VALUES ('Jack n the box jumbo burger', true);
+INSERT INTO burgers (name, isDevour) VALUES ('Happy meal burger', true);
+INSERT INTO burgers (name, isDevour) VALUES ('The Yankee', true);
+INSERT INTO burgers (name) VALUES ('BB Burger');
