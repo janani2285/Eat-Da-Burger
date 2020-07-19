614085a1ecf99b6b2e75efa549e1bcfabcf4faab
Janani NA <janani2285@gmail.com>
2020-07-18 18:49:05 -0700

    connection params updated for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
+let connection;
 
 //database details
-const connection = mysql.createConnection({
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "password",
   database: "burger_db",
 });
+}
 
 // Connecting to database
 connection.connect((err) => {
